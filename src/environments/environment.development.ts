beb1c08cfd21de4938aa13b6d119536fa294124a
paridaabinash <abinashparida833@gmail.com>
2024-11-14 20:00:28 +0530

    url change on package.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  local_apiUrl: "http://admin:admin@localhost:3000/api",
+  prod_apiUrl: "https://oms-ph-server-production.up.railway.app/",
+};
