43db238832b1ec8322bd576bbf9bdaa34e8d8e13
paridaabinash <abinashparida833@gmail.com>
2024-11-15 18:20:27 +0530

    Ignored cache and build folders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   local_apiUrl: "http://admin:admin@localhost:3000/api",
-  prod_apiUrl: "https://oms-ph-server-production.up.railway.app/",
+  prod_apiUrl: "https://oms-ph-server-production.up.railway.app/api",
 };
